4a3c148e96dd38a1374e8095bcf4146925eb9b5a
Felipe Pinheiro <flpinheiro@gmail.com>
2022-06-23 22:32:41 -0300

    update add apollo graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-function App() {
+import { gql, useQuery } from "@apollo/client";
 
+const GET_LESSON_QUERY = gql`
+query {
+  lessons{
+    id
+    title
+  }
+}`
+
+interface Lesson {
+  id: string,
+  title: string,
+}
+function App() {
+  const { data } = useQuery<{ Lessons: Lesson[] }>(GET_LESSON_QUERY)
+
+  console.log(data);
   return (
     <>
-     <h1 className="title">Hello World</h1>
+      <h1 className="text-5xl text-zinc-500">Hello World Vite</h1>
+      <ul>
+        {data?.Lessons.map(lesson => {
+          return (<li>{lesson.title}</li>);
+        })}
+      </ul>
     </>
   )
 }
